229c7047d943856f66733add680877a927385696
Kyle Miller <kylesmiller38@gmail.com>
2019-06-09 16:51:37 -0600

    created getAll method

--- src/ServerQuerier.py
+++ src/ServerQuerier.py
@@ -32,17 +32,33 @@ class ServerQuerier:
     def getMaxPlayers(self):
         return self.__maxPlayers
 
+    def getPlayerList(self):
+        return self.__playerList
+
     def getPopulation(self):
         """
         return a formatted string of server population
-        :return: (currentPlayers/maxPlayers)
+        :return: currentPlayers/maxPlayers
         """
-        return "(" + str(self.__currentPlayers) + "/" + str(self.__maxPlayers) + ")"
+        return str(self.__currentPlayers) + "/" + str(self.__maxPlayers)
+
+    def getAll(self):
+        """
+        Return a formatted string of all server information
+        :return:  all server info formatted
+        """
+        allInfo = ("**__" + str(self.getName()) + "__** is playing **__" + str(self.getMap()) +
+               "**__ with a population of **__(" + str(self.getPopulation()) + ")__**\n" +
+               "**__PLAYER LIST:**__\n")
+        for player in self.getPlayerList():
+            allInfo += str(player) + ", "
+        return allInfo
 
     def query(self):
         """
         Query the steam server
         """
+        self.__playerList = []  # clear the playerList
         serverAddress = (self.__serverIP, self.__queryPort)
         with valve.source.a2s.ServerQuerier(serverAddress) as server:
             info = server.info()
@@ -51,4 +67,18 @@ class ServerQuerier:
             self.__map = "{map}".format(**info)
             self.__currentPlayers = "{player_count}".format(**info)
             self.__maxPlayers = "{max_players}".format(**info)
-            self.__playerList = server.players()
+            for player in server.players()["players"]:
+                self.__playerList.append(player)
+        self.__writeToDictionary()
+
+    def __writeToDictionary(self):
+        """
+        Write all gathered data to a dictionary that can be converted into a .json file
+        :return:  the created dictionary
+        """
+        pass
+
+
+# test = ServerQuerier("66.151.138.224", 13305)
+# test.query()
+# print(test.getPlayerList())
