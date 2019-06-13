65199c203f928bbf58e5706d9db8e3fa8195504e
Kyle Miller <kylesmiller38@gmail.com>
2019-06-12 19:19:33 -0600

    set up 120 second interval timer with proper input and output .json files

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 """
 Run the server querier
 """
-from QueryManager import MultiQuerier
+from QueryManager import QueryManager
 
-serverQuerier = MultiQuerier(
+serverQuerier = QueryManager(
                              "C:\\Users\\raysp\\Desktop\\Python\\Personal\\BirbBot\\resources\\ServerConstructorInfo.json",
                              "C:\\Users\\raysp\\Desktop\\Python\\Personal\\SteamServerQuerier\\src\\ServerQueryData.json",
                              )
 
 serverQuerier.queryServers()
-serverQuerier.start(queryInterval=20.0, writeToJson=True)
+serverQuerier.start(queryInterval=120.0, writeToJson=True)
